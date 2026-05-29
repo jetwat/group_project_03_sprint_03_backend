d4f47a5daef75a3758bba6f1e7334d60cafa7833
Sahatsawat-Wattana <sahatsawat.wattanapraditchai@gmail.com>
2026-05-29 15:42:32 +0700

    debug authorize

--- src/middlewares/author.js
+++ src/middlewares/author.js
@@ -1,9 +1,9 @@
-import {User} from "../modules/users/user.model"
+import { User } from '../modules/users/user.model';
 
-export const authorizeAdmin = (req,res,next) => {
-    const role = await User.findById(req.user.users._id)
-    if (role !== 'admin') {
-        return res.status(403).json({success:false,message:"Forbidden"})
-    }
-    next();
-}
+export const authorizeAdmin = async (req, res, next) => {
+  const role = await User.findById(req.user.users._id);
+  if (role !== 'admin') {
+    return res.status(403).json({ success: false, message: 'Forbidden' });
+  }
+  next();
+};
