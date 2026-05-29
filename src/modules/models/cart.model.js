63e23dbe11a545c4c688410d789215ef9e184f18
phongphon1611 <phongphon.js@gmail.com>
2026-05-29 10:32:16 +0700

    createCart.controller

--- src/modules/models/cart.model.js
+++ src/modules/models/cart.model.js
@@ -1,54 +1,54 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const cart_ItemSchema = new mongoose.Schema(
   {
     user_id: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
+      ref: 'User',
+      required: true
     },
     total_amount: {
       type: Decimal128,
       required: true,
-      trim: true,
+      trim: true
     },
     cart_item: [
       {
         book_id: {
           type: mongoose.Schema.Types.ObjectId,
-          ref: "Product",
-          required: true,
+          ref: 'Product',
+          required: true
         },
         book_name: {
           type: string,
           required: true,
           unique: true,
-          trim: true,
+          trim: true
         },
         author: {
           type: string,
           required: true,
           unique: true,
-          trim: true,
+          trim: true
         },
         quantity: {
           type: integer,
           required: true,
-          trim: true,
+          trim: true
         },
         price: {
           type: Decimal128,
           required: true,
-          trim: true,
+          trim: true
         },
         img_link: {
           type: string,
           required: true,
-          trim: true,
-        },
-      },
-    ],
+          trim: true
+        }
+      }
+    ]
   },
-  { timestamps: true },
+  { timestamps: true }
 );
-export const User = mongoose.model("user", userSchema);
+export const Cart = mongoose.model('cart', cart_ItemSchema);
