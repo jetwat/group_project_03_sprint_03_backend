d1944a4d9df291e69fb0737ad288f09b68d3b58f
Sahatsawat-Wattana <sahatsawat.wattanapraditchai@gmail.com>
2026-06-02 16:48:46 +0700

    debug routes

--- src/routes/feedback.routes.js
+++ src/routes/feedback.routes.js
@@ -4,4 +4,5 @@ import { createFeedback } from '../modules/setting/feedback.controller.js';
 
 export const router = express.Router();
 
+router.get('/', authUser, createFeedback);
 router.post('/', authUser, createFeedback);
